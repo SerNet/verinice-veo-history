0d3582f98ddc1fc0779ccb21f214c0950c8b451a
Jonas Jordan <jj@sernet.de>
2021-10-28 11:16:30 +0200

    close migration statments

--- src/main/kotlin/org/veo/history/migrations/V1__create.kt
+++ src/main/kotlin/org/veo/history/migrations/V1__create.kt
@@ -22,29 +22,29 @@ import org.flywaydb.core.api.migration.Context
 
 class V1__create : BaseJavaMigration() {
     override fun migrate(context: Context) {
-        context.connection.createStatement().execute(
-            """
+        context.connection.createStatement().use {
+            it.execute(
+                """
+                create table revision (
+                   id int8 not null,
+                    author varchar(255),
+                    change_number int8 not null,
+                    client_id uuid,
+                    content jsonb,
+                    time timestamp,
+                    type int4,
+                    uri bytea,
+                    primary key (id)
+                );
             
-    create table revision (
-       id int8 not null,
-        author varchar(255),
-        change_number int8 not null,
-        client_id uuid,
-        content jsonb,
-        time timestamp,
-        type int4,
-        uri bytea,
-        primary key (id)
-    );
-
-    alter table revision 
-       add constraint UK_uri_change_number unique (uri, change_number);
-
-    create sequence hibernate_sequence start 1 increment 1;
-
-    CREATE INDEX revision_content_owner ON revision USING HASH((content -> 'owner' ->> 'targetUri'));
-
-"""
-        )
+                alter table revision 
+                   add constraint UK_uri_change_number unique (uri, change_number);
+            
+                create sequence hibernate_sequence start 1 increment 1;
+            
+                CREATE INDEX revision_content_owner ON revision USING HASH((content -> 'owner' ->> 'targetUri'));
+                """
+            )
+        }
     }
 }
