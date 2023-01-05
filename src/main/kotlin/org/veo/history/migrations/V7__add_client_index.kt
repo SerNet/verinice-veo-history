2ec209b31771de60f244c4244f2e8fff00999428
Jonas Jordan <jj@sernet.de>
2023-01-05 09:39:22 +0100

    add DB index revision_client_id
    VEO-1838

---
+++ src/main/kotlin/org/veo/history/migrations/V7__add_client_index.kt
@@ -0,0 +1,33 @@
+/**
+ * verinice.veo history
+ * Copyright (C) 2023  Jonas Jordan
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.veo.history.migrations
+
+import org.flywaydb.core.api.migration.BaseJavaMigration
+import org.flywaydb.core.api.migration.Context
+
+class V7__add_client_index : BaseJavaMigration() {
+    override fun migrate(context: Context) {
+        context.connection.createStatement().use {
+            it.execute(
+                """
+                create index revision_client_id on revision (client_id);
+                """,
+            )
+        }
+    }
+}
