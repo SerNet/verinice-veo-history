fae660e510af10b3a7db40d5c3fa6191eaee5a0d
Jochen Kemnade <jk@sernet.de>
2024-08-13 13:25:23 +0200

    enable Gradle build cache
    verinice-veo#1116

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,3 +10,18 @@ plugins {
 }
 
 rootProject.name = "veo-history"
+
+val isCiServer = System.getenv().containsKey("CI")
+
+buildCache {
+    local {
+        isEnabled = !isCiServer
+    }
+    System.getenv("GRADLE_REMOTE_BUILD_CACHE_URL")?.let { url ->
+        remote<HttpBuildCache> {
+            this.url = uri(url)
+            isPush = isCiServer
+            isAllowUntrustedServer = isCiServer
+        }
+    }
+}
