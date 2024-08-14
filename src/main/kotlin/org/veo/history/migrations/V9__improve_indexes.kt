649b05c097261a76ca7ce20f47696b30cad430d5
Jochen Kemnade <jk@sernet.de>
2024-08-14 14:59:43 +0200

    improve database indexes
    verinice-veo#3025

---
+++ src/main/kotlin/org/veo/history/migrations/V9__improve_indexes.kt
@@ -0,0 +1,36 @@
+/**
+ * verinice.veo history
+ * Copyright (C) 2024  Jochen Kemnade
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.veo.history.migrations
+
+import org.flywaydb.core.api.migration.BaseJavaMigration
+import org.flywaydb.core.api.migration.Context
+
+@SuppressWarnings("ClassName")
+class V9__improve_indexes : BaseJavaMigration() {
+    override fun migrate(context: Context) {
+        context.connection.createStatement().use {
+            it.execute(
+                """
+                DROP INDEX revision_content_owner;
+                CREATE INDEX revision_client_author_owner ON revision (client_id, author, (content -> 'owner' ->> 'targetUri'));
+                CREATE INDEX revision_uri_type ON revision (uri, type);
+                """,
+            )
+        }
+    }
+}
